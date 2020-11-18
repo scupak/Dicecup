aad7bf1f2bbc20fcdecf574a2ee6fbd93469b605
oleeriksen <oe@easv.dk>
2020-11-18 23:07:31 +0100

    completed

--- app/src/main/java/easv/oe/dicecup/MainActivity.kt
+++ app/src/main/java/easv/oe/dicecup/MainActivity.kt
@@ -9,6 +9,7 @@ import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
+    // mapping from 1..6 to drawables, the first index is unused
     val diceId = intArrayOf(0, R.drawable.dice1,
                                R.drawable.dice2,
                                R.drawable.dice3,
@@ -16,7 +17,7 @@ class MainActivity : AppCompatActivity() {
                                R.drawable.dice5,
                                R.drawable.dice6)
 
-    val ran = Random()
+    val mRandomGenerator = Random()
 
     val mHistory = mutableListOf<Pair<Int, Int>>()
 
@@ -28,22 +29,35 @@ class MainActivity : AppCompatActivity() {
 
         findViewById<ImageView>(R.id.imgDice2).setOnClickListener { v -> onClickRoll() }
         findViewById<ImageView>(R.id.imgDice1).setOnClickListener { v -> onClickRoll() }
+        findViewById<Button>(R.id.btnClear).setOnClickListener { v -> onClickClear() }
     }
 
     fun onClickRoll(){
-        val e1 = ran.nextInt(6) + 1
-        val e2 = ran.nextInt(6) + 1
-        mHistory.add(Pair(e1,e2))
-        if (mHistory.size > 5) mHistory.removeAt(0)
+        val e1 = mRandomGenerator.nextInt(6) + 1
+        val e2 = mRandomGenerator.nextInt(6) + 1
+
+        // update dices
         val d1 = findViewById<ImageView>(R.id.imgDice1)
         val d2 = findViewById<ImageView>(R.id.imgDice2)
         d1.setImageResource( diceId[e1] )
         d2.setImageResource( diceId[e2] )
+
+        //update history
+        mHistory.add(Pair(e1,e2))
+        if (mHistory.size > 5) mHistory.removeAt(0)
+        updateHistory()
+    }
+
+    fun onClickClear() {
+        mHistory.clear()
+        updateHistory()
+    }
+
+    // ensures that the history text aligns the history object
+    fun updateHistory() {
         val tvHistory = findViewById<TextView>(R.id.tvHistory)
-        var s:String = ""
-        mHistory.forEach { p ->  val e1 = p.first
-                                 val e2 = p.second
-                                 s += "$e1 - $e2 \n"}
+        var s = ""
+        mHistory.forEach { p ->  val e1 = p.first; val e2 = p.second; s += "$e1 - $e2 \n" }
         tvHistory.text = s
     }
 
